b906560bf1f31436781dc325ac3267df8f9077f3
Andrey Aleksandrov <andraway@mail.ru>
2019-10-21 17:38:32 +0300

    add payment id to db

---
+++ Common/Migrations/20191021143801_AddPaymentId.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Common.Migrations
+{
+    public partial class AddPaymentId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "payment_id",
+                table: "account_history",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "payment_id",
+                table: "account_history");
+        }
+    }
+}
